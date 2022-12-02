8095f718e1e9f474e0436cacf0375d87b6d48b61
Andreia Cifoni <andreiacifoni@gmail.com>
2022-12-02 17:29:44 -0500

    get and delete routes created

--- BackEnd/routes/index.js
+++ BackEnd/routes/index.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const router = Router();
 const pool = require("../db/index");
 
+//-------------------ACTIVITIES------------------
 router.get("/", (req, res) => {
   pool.query("SELECT * FROM activities", (error, results) => {
     if (error) throw error;
@@ -9,12 +10,48 @@ router.get("/", (req, res) => {
   });
 });
 
-router.post("/", (req, res) => {});
+router.post("/", (req, res) => {
+  //check if title already exist
+  //query to post activity
+});
 
-router.get("/:id", (req, res) => {});
+router.get("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  pool.query(
+    "SELECT * FROM activities WHERE id = $1",
+    [id],
+    (error, results) => {
+      if (error) throw error;
+      res.status(200).json(results.rows);
+    }
+  );
+});
 
-router.put("/:id", (req, res) => {});
+router.put("/:id", (req, res) => {
+  //check if id exist
+  //update ativity
+});
 
-router.delete("/:id", (req, res) => {});
+router.delete("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  pool.query(
+    "SELECT * FROM activities WHERE id = $1",
+    [id],
+    (error, results) => {
+      const noActivityFound = !results.rows.length;
+      if (noActivityFound) {
+        res.send("No activity found in database!");
+      }
+      pool.query(
+        "DELETE FROM activities WHERE id = $1",
+        [id],
+        (error, results) => {
+          if (error) throw error;
+          res.status(200).send("Activity removed successfully!");
+        }
+      );
+    }
+  );
+});
 
 module.exports = router;
