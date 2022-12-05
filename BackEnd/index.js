7c88b4f71413cb99001e338267521bc1b62f68b6
Andreia Cifoni <andreiacifoni@gmail.com>
2022-12-05 13:38:51 -0500

    creating routes

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,5 +1,13 @@
 const express = require("express");
+const streetsOfCanadaRoutes = require("./routes/index");
 const app = express();
 const port = 3000;
+
+app.use(express.json());
+app.use("/", streetsOfCanadaRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
--- BackEnd/routes/index.js
+++ BackEnd/routes/index.js
@@ -2,6 +2,42 @@ const { Router } = require("express");
 const router = Router();
 const pool = require("../db/index");
 
+//-------------------USERS------------------
+router.post("/register", (req, res) => {
+  const { name, email, password } = req.body;
+  //check if email already exists
+  pool.query(
+    "SELECT * FROM users WHERE email = $1",
+    [email],
+    (error, results) => {
+      if (results.rows.length) {
+        res.send("User already exist!");
+      }
+      //query to post user
+      pool.query(
+        "INSERT INTO users (name, email, password) VALUES ($1, $2, $3)",
+        [name, email, password],
+        (error, results) => {
+          if (error) throw error;
+          res.status(201).send("User created successfully!");
+        }
+      );
+    }
+  );
+});
+
+router.get("/user", (req, res) => {
+  const { name, password } = req.body;
+  pool.query(
+    "SELECT * FROM users WHERE name = $1 AND password = $2",
+    [name, password],
+    (error, results) => {
+      if (error) throw error;
+      res.status(200).send(`Hello ${name}`).json(results.rows);
+    }
+  );
+});
+
 //-------------------ACTIVITIES------------------
 router.get("/", (req, res) => {
   pool.query("SELECT * FROM activities", (error, results) => {
@@ -10,9 +46,27 @@ router.get("/", (req, res) => {
   });
 });
 
-router.post("/", (req, res) => {
-  //check if title already exist
-  //query to post activity
+router.post("/new", (req, res) => {
+  const { title, description, photo, date_created, user_id } = req.body;
+  //check if title already exists
+  pool.query(
+    "SELECT * FROM activities WHERE title = $1",
+    [title],
+    (error, results) => {
+      if (results.rows.length) {
+        res.send("Activity already exists!");
+      }
+      //query to post user
+      pool.query(
+        "INSERT INTO activities (title, description, photo, date_created, user_id) VALUES ($1, $2, $3, $4, $5)",
+        [title, description, photo, date_created, user_id],
+        (error, results) => {
+          if (error) throw error;
+          res.status(201).send("Activity created successfully!");
+        }
+      );
+    }
+  );
 });
 
 router.get("/:id", (req, res) => {
