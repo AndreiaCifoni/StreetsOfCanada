25099d24f25053e5c69ab292bcf02f81181fe0a1
Andreia Cifoni <andreiacifoni@gmail.com>
2022-12-07 17:36:21 -0500

    some routes comments

--- BackEnd/routes/index.js
+++ BackEnd/routes/index.js
@@ -113,4 +113,86 @@ router.delete("/activities/:id", (req, res) => {
 
 //-------------------COMMENTS------------------
 
+router.get("/activities/:id/comments", (req, res) => {
+  const id = parseInt(req.params.id);
+  pool.query(
+    "SELECT * FROM comments WHERE activity_id = $1",
+    [id],
+    (error, results) => {
+      if (error) throw error;
+      res.status(200).json(results.rows);
+    }
+  );
+});
+
+router.post("/activities/:id/comments", (req, res) => {
+  const { user_id, activity_id, comment, rating } = req.body;
+  pool.query(
+    "INSERT INTO comments (user_id, activity_id, comment, rating ) VALUES ($1, $2, $3, $4)",
+    [user_id, activity_id, comment, rating],
+    (error, results) => {
+      if (error) throw error;
+      res.status(201).send("Comment created successfully!");
+    }
+  );
+});
+
+// router.get("/activities/:id/comments/:id", (req, res) => {
+//   const id = parseInt(req.params.id);
+//   pool.query(
+//     "SELECT * FROM activities WHERE activity_id = $1",
+//     [id],
+//     (error, results) => {
+//       if (error) throw error;
+//       res.status(200).json(results.rows);
+//     }
+//   );
+// });
+
+// router.put("/activities/:id/comments/:id", (req, res) => {
+//   const id = parseInt(req.params.id);
+//   const { activity_id, title, description, photo } = req.body;
+//   //check if id exists...
+//   pool.query(
+//     "SELECT * FROM activities WHERE activity_id = $1",
+//     [id],
+//     (error, results) => {
+//       const noActivityFound = !results.rows.length;
+//       if (noActivityFound) {
+//         res.send("Activity does not exist in the database");
+//       }
+//       pool.query(
+//         "UPDATE activities SET title = $2, description = $3, photo = $4  WHERE activity_id = $1",
+//         [activity_id, title, description, photo],
+//         (error, results) => {
+//           if (error) throw error;
+//           res.status(200).send("Activity updated successfully!");
+//         }
+//       );
+//     }
+//   );
+// });
+
+router.delete("/activities/:id/comments/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  pool.query(
+    "SELECT * FROM comments WHERE comment_id = $1",
+    [id],
+    (error, results) => {
+      const noCommentFound = !results.rows.length;
+      if (noCommentFound) {
+        res.send("No comment found in database!");
+      }
+      pool.query(
+        "DELETE FROM comments WHERE comment_id = $1",
+        [id],
+        (error, results) => {
+          if (error) throw error;
+          res.status(200).send("Activity removed successfully!");
+        }
+      );
+    }
+  );
+});
+
 module.exports = router;
