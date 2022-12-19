b939415ba50e99cde10625514dff1c3bf79bee55
Andreia Cifoni <andreiacifoni@gmail.com>
2022-12-19 13:56:53 -0500

    individual activity -starting aligment and content

--- FrontEnd/src/components/activities/Activity.js
+++ FrontEnd/src/components/activities/Activity.js
@@ -4,21 +4,27 @@ import "../../layout.css";
 const Activity = () => {
   return (
     <div className="activity-container">
-      <div>
+      <div className="activity-column">
         <img
           className="activity-img"
           src="https://images.unsplash.com/photo-1519331379826-f10be5486c6f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
           alt="Park"
         />
-        <p>User Name</p>
-        <p>Date created</p>
+        <p className="activity-small-text">User Name</p>
+        <p className="activity-small-text">Date created</p>
         <button>Edit</button>
         <button>Delete</button>
       </div>
-      <div>
+      <div className="activity-column">
         <h1>Title</h1>
-        <p>Location</p>
-        <p>description</p>
+        <p className="activity-small-text">Location</p>
+        <p className="activity-medium-text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et nisi
+          tortor. Aliquam erat volutpat. Fusce non massa sed ligula elementum
+          consectetur. Morbi in metus id dolor vestibulum placerat. Aliquam
+          lectus lorem, sodales a ipsum nec, consectetur interdum nisl. Nam
+          interdum sit amet quam sed pellentesque.
+        </p>
         {/* <div>Here goes the comment box</div> */}
       </div>
     </div>
