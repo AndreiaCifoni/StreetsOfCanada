81f33f2fbca327638ecfe59a7f5e11f8ca6fc6ff
Andreia Cifoni <andreiacifoni@gmail.com>
2023-01-11 16:14:15 -0500

    ReviewForm - star rating added

--- FrontEnd/src/components/reviews/ReviewForm.js
+++ FrontEnd/src/components/reviews/ReviewForm.js
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import { Rating } from "react-simple-star-rating";
 
 const ReviewForm = () => {
+  const [rating, setRating] = useState(0);
+
+  const handleRating = (rate) => {
+    setRating(rate);
+  };
+
+  const handleResetReview = () => {
+    setRating(0);
+  };
+
+  console.log(rating);
+
   return (
     <form
       onSubmit={(e) => {
@@ -9,10 +22,19 @@ const ReviewForm = () => {
       }}
     >
       <div className="py-2 px-4 rounded border-solid border-2 border-indigo-400 shadow">
-        <label>Leave our rating:</label>
+        <div className="mb-2">
+          <label className="mr-4">Leave our rating:</label>
+          <Rating
+            SVGclassName="inline-block"
+            onClick={handleRating}
+            initialValue={rating}
+            fillColor="#4E5AC7"
+            size={25}
+          />
+        </div>
         <div>
           <textarea
-            className="w-11/12 h-20"
+            className="w-11/12 h-20 py-1 px-2"
             type="text"
             //value={review.review}
             // onChange={(e) =>
@@ -24,6 +46,7 @@ const ReviewForm = () => {
           className="mt-2 py-0.5 px-1.5 rounded border-solid border-2 border-indigo-400 hover:border-violet-400 hover:bg-violet-300 shadow"
           type="submit"
           value="Submit"
+          onClick={handleResetReview}
         />
       </div>
     </form>
