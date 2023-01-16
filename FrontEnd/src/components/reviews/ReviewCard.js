c11381aa23c8272695ceca4017e0d6c0ab4c4401
Andreia Cifoni <andreiacifoni@gmail.com>
2023-01-15 22:40:43 -0500

    ReviewEdit - working!

--- FrontEnd/src/components/reviews/ReviewCard.js
+++ FrontEnd/src/components/reviews/ReviewCard.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { Rating } from "react-simple-star-rating";
+import ReviewEdit from "./ReviewEdit";
 
 const ReviewCard = ({
   user,
@@ -10,6 +11,8 @@ const ReviewCard = ({
   setReviewList,
   reviewList,
 }) => {
+  const [isEditingReview, setIsEditingReview] = useState(false);
+
   const onClickDeleteReview = async () => {
     try {
       const deleteReviews = await fetch(
@@ -27,36 +30,62 @@ const ReviewCard = ({
     }
   };
 
+  const onClickEditReview = () => {
+    setIsEditingReview(true);
+  };
+
+  const onClickCancelEditReview = () => {
+    setIsEditingReview(false);
+  };
+
   return (
     <div className="my-2 py-2 px-4 rounded border-solid border-2 border-indigo-400 shadow">
-      <div>
-        <p className="flex justify-between">
-          <span>{user}</span>
-          <span className="text-xs">{date.substring(0, 10)}</span>
-        </p>
-
+      {isEditingReview ? (
+        <ReviewEdit
+          onClickCancelEditReview={onClickCancelEditReview}
+          user={user}
+          rating={rating}
+          date={date}
+          review={review}
+          reviewId={reviewId}
+          setReviewList={setReviewList}
+          reviewList={reviewList}
+        />
+      ) : (
         <div>
-          <Rating
-            SVGclassName="inline-block"
-            initialValue={rating}
-            readonly
-            fillColor="#4E5AC7"
-            size={25}
-          />
+          <div>
+            <p className="flex justify-between">
+              <span>{user}</span>
+              <span className="text-xs">{date.substring(0, 10)}</span>
+            </p>
+
+            <div>
+              <Rating
+                SVGclassName="inline-block"
+                initialValue={rating}
+                readonly
+                fillColor="#4E5AC7"
+                size={25}
+              />
+            </div>
+            <p>{review}</p>
+          </div>
+          <div>
+            <button
+              className="mt-4 py-0.5 px-1.5 rounded border-solid border-2 border-indigo-400 hover:border-violet-400 hover:bg-violet-300 shadow"
+              onClick={onClickEditReview}
+            >
+              Edit
+            </button>
+            <button
+              className="mt-4 mx-3 py-0.5 px-1.5 rounded border-solid border-2 border-indigo-400 hover:border-violet-400 hover:bg-violet-300 shadow"
+              onClick={onClickDeleteReview}
+            >
+              Delete
+            </button>
+          </div>
         </div>
-        <p>{review}</p>
-      </div>
-      <div>
-        <button className="mt-4 py-0.5 px-1.5 rounded border-solid border-2 border-indigo-400 hover:border-violet-400 hover:bg-violet-300 shadow">
-          Edit
-        </button>
-        <button
-          className="mt-4 mx-3 py-0.5 px-1.5 rounded border-solid border-2 border-indigo-400 hover:border-violet-400 hover:bg-violet-300 shadow"
-          onClick={onClickDeleteReview}
-        >
-          Delete
-        </button>
-      </div>
+      )}
     </div>
   );
 };
---
+++ FrontEnd/src/components/reviews/ReviewEdit.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import ReviewForm from "./ReviewForm";
+
+const ReviewEdit = ({
+  onClickCancelEditReview,
+  user,
+  rating,
+  date,
+  review,
+  reviewId,
+  setReviewList,
+  reviewList,
+}) => {
+  const [editReview, setEditReview] = useState({
+    user_id: 1,
+    review: review,
+    rating: rating,
+  });
+
+  const onSubmitEditReview = async () => {
+    try {
+      await fetch(`http://localhost:3000/reviews/${reviewId}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          user_id: 1,
+          review: editReview.review,
+          rating: editReview.rating,
+        }),
+      });
+      const editedReviewList = reviewList.map((review) => {
+        if (review.review_id === reviewId) {
+          review.review = editReview.review;
+          review.rating = editReview.rating;
+          return review;
+        }
+        return review;
+      });
+      setReviewList(editedReviewList);
+      onClickCancelEditReview();
+    } catch {
+      alert("Error");
+    }
+  };
+
+  return (
+    <div>
+      <ReviewForm
+        review={editReview}
+        setReview={setEditReview}
+        onSubmitReview={onSubmitEditReview}
+      />
+      <button
+        className="mt-4 py-0.5 px-1.5 rounded border-solid border-2 border-indigo-400 hover:border-violet-400 hover:bg-violet-300 shadow"
+        onClick={onClickCancelEditReview}
+      >
+        Cancel
+      </button>
+    </div>
+  );
+};
+
+export default ReviewEdit;
