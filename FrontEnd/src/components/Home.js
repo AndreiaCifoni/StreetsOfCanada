79f00eec4558bdb90505e039e0ba86258fa1380d
Andreia Cifoni <andreiacifoni@gmail.com>
2023-01-28 12:42:02 -0500

    some design in Home

--- FrontEnd/src/components/Home.js
+++ FrontEnd/src/components/Home.js
@@ -4,13 +4,13 @@ import ActivityList from "./activities/ActivityList";
 const Home = () => {
   return (
     <div className="mt-8 mx-16 bg-indigo-100 rounded py-2">
-      <div className="mx-8 my-4 h-96 bg-[url('https://res.cloudinary.com/deiacifoni/image/upload/v1672176321/projects/StreetsOfCanada/streets_of_Canada_4_knnwxz.jpg')]">
-        <h1 className="inline-block my-36 mx-48 text-8xl font-bold bg-orange-50/90 px-5 py-0.5 ">
+      <div className="flex justify-center items-center mx-8 my-4 h-96 bg-[url('https://res.cloudinary.com/deiacifoni/image/upload/v1672176321/projects/StreetsOfCanada/streets_of_Canada_4_knnwxz.jpg')]">
+        <h1 className="inline-block text-center text-8xl xl:text-7xl lg:text-5xl md:w-1/2 sm:w-4/5 font-bold bg-orange-50/90 px-5 py-0.5 ">
           Streets Of Canada
         </h1>
       </div>
-      <div className="w-2/3 mx-auto my-12">
-        <p className="text-lg text-center">
+      <div className="w-2/3 lg:w-4/5 mx-auto my-12 ">
+        <p className="text-xl lg:text-lg md:text-base  text-center">
           Streets of Canada is a website that has the perfect activity for you!
           This page is for anyone interested in post or review experiences
           throughout the city. If you enjoyed hiking in your neighborhood park,
