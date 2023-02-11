6a5888c979aa6b87102fa633589823d32112acbe
Andreia Cifoni <andreiacifoni@gmail.com>
2023-02-10 19:25:03 -0500

    global variable - apiURL

--- FrontEnd/src/globalVariables.js
+++ FrontEnd/src/globalVariables.js
@@ -1,7 +1,7 @@
 //use this one to run with deployed BackEnd
-const apiURL = "https://streets-of-canada.onrender.com";
+//const apiURL = "https://streets-of-canada.onrender.com";
 
 //use this one to run locally
-//const apiURL = "http://localhost:3000";
+const apiURL = "http://localhost:3000";
 
 export { apiURL };
