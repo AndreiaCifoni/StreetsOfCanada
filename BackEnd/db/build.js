9c8899842a250677c32337cbe29f67d50dcbc355
Andreia Cifoni <andreiacifoni@gmail.com>
2023-03-08 09:34:58 -0500

    change back the env variables on pool and buld.js

--- BackEnd/db/build.js
+++ BackEnd/db/build.js
@@ -1,19 +1,20 @@
 const Pool = require("pg").Pool;
 const fs = require("fs");
-const connectionString = process.env.DATABASE_URL;
-const pool = new Pool({
-  connectionString,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
+//next lines were added during deployment on Heroku, but failed
+// const connectionString = process.env.DATABASE_URL;
 // const pool = new Pool({
-//   user: process.env.DBUSERNAME || "postgres",
-//   host: process.env.DBHOSTNAME || "localhost",
-//   database: process.env.DBDATABASE || "streets_of_canada",
-//   password: process.env.DBPASSWORD || "1234",
-//   port: process.env.DBPORT || 5432,
+//   connectionString,
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
 // });
+const pool = new Pool({
+  user: process.env.DBUSERNAME || "postgres",
+  host: process.env.DBHOSTNAME || "localhost",
+  database: process.env.DBDATABASE || "streets_of_canada",
+  password: process.env.DBPASSWORD || "1234",
+  port: process.env.DBPORT || 5432,
+});
 
 const sql = fs.readFileSync("./db/schema.sql").toString();
 
--- BackEnd/db/pool.js
+++ BackEnd/db/pool.js
@@ -1,18 +1,18 @@
 const Pool = require("pg").Pool;
-const connectionString = process.env.DATABASE_URL;
-
-const pool = new Pool({
-  connectionString,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
+//next lines were added during deployment on Heroku, but failed
+// const connectionString = process.env.DATABASE_URL;
 // const pool = new Pool({
-//   user: process.env.DBUSERNAME || "postgres",
-//   host: process.env.DBHOSTNAME || "localhost",
-//   database: process.env.DBDATABASE || "streets_of_canada",
-//   password: process.env.DBPASSWORD || "1234",
-//   port: process.env.DBPORT || 5432,
+//   connectionString,
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
 // });
+const pool = new Pool({
+  user: process.env.DBUSERNAME || "postgres",
+  host: process.env.DBHOSTNAME || "localhost",
+  database: process.env.DBDATABASE || "streets_of_canada",
+  password: process.env.DBPASSWORD || "1234",
+  port: process.env.DBPORT || 5432,
+});
 
 module.exports = pool;
