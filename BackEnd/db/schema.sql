91e712c92cf9a7fc3963cc65e0a875ec70e08e5d
Andreia Cifoni <andreiacifoni@gmail.com>
2023-04-20 15:59:57 -0400

    changes in schema - real activity description

--- BackEnd/db/schema.sql
+++ BackEnd/db/schema.sql
@@ -43,7 +43,7 @@ CREATE TABLE IF NOT EXISTS cities (
     REFERENCES provinces (province_id)
   );
 
-INSERT INTO cities(name, province_id) VALUES ('Toronto', 'ON'),('Vancouver','BC'),('Calgary', 'AB'),('Charlottetown','PE');
+INSERT INTO cities(name, province_id) VALUES ('Toronto', 'ON'),('Charlottetown','PE');
   
 CREATE TABLE IF NOT EXISTS activities (
   activity_id SERIAL,
@@ -64,21 +64,21 @@ CREATE TABLE IF NOT EXISTS activities (
   );
 
 INSERT INTO activities(title, description, address, latitude, longitude, photo, user_id, city_id) VALUES ('Craigleigh Gardens Dog Park',
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam semper eu lectus ac maximus. Sed non posuere arcu. Fusce pharetra arcu justo, vel mollis ante fermentum nec. Fusce commodo aliquam consequat. Phasellus ut ipsum vel diam accumsan volutpat. Vestibulum sit amet felis laoreet, faucibus est in, commodo enim. Sed ultricies urna vitae elit luctus molestie.',
+    'A quiet 3.4 hectare park near Castle Frank Road and Bloor Street East featuring an entrance with ornamental gates, a mature tree canopy and a dog off-leash area. The park is adjacent to Milkmans Lane with access to a ravine trail and the Don Valley Brick Works.',
     'Milkmans Lane',
     43.67748558012985,
     -79.37223787483332,
     'https://images.unsplash.com/photo-1534361960057-19889db9621e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGRvZyUyMHBhcmt8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
      1,
     1), ('Brighton Beach Lighthouse',
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam semper eu lectus ac maximus. Sed non posuere arcu. Fusce pharetra arcu justo, vel mollis ante fermentum nec. Fusce commodo aliquam consequat. Phasellus ut ipsum vel diam accumsan volutpat. Vestibulum sit amet felis laoreet, faucibus est in, commodo enim. Sed ultricies urna vitae elit luctus molestie.',
+    'Brighton Beach Front Range Lighthouse is a 12.2 metre (40 ft) square, tapered light tower, and features white-painted shingles with red accents and lantern room; a decorative maple leaf is painted on two faces of the lantern room. It sits along the shoreline of the North (Yorke) River in the residential community of Brighton Ward of the City of Charlottetown in Prince Edward Island.',
     '160 York Ln',
     46.23060602367885,
     -63.146939255098474,
     'https://images.unsplash.com/photo-1522679056866-8dbbc8774a9d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGlnaHRob3VzZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
     2,
     4), ('Graffiti Alley',
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer aliquam finibus dolor, vitae placerat tortor porta id. Fusce et nulla egestas, tempus felis in, tempus ipsum. Donec a urna maximus, maximus sapien id, elementum metus. Integer faucibus rutrum lacus, egestas posuere augue malesuada ut. Proin semper risus sit amet nulla tristique, ut lobortis tellus imperdiet. Cras egestas orci quis mauris tincidunt tempus. Nullam sodales nunc purus, ac varius ante gravida vel. Sed eu turpis dapibus, vestibulum erat quis, sagittis urna. In lobortis urna a tincidunt dictum. Etiam sit amet libero sed diam ultricies imperdiet. In hac habitasse platea dictumst.',
+    'Graffiti Alley runs parallel to the trendy stretch of Queen Street West. It encompasses three city blocks and includes the contiguous alleyway of Rush Lane. It is a popular spot to view some of the best examples of a vibrant street art and mural culture from Toronto. Graffiti Alley is a popular backdrop for photoshoots, as well as a place to see works by iconic artists like Duro the Third, uber5000, and ELICSER. It has served as the venue for small street festivals, a set for music videos, and for a time, had a restaurant named after it.',
     'Graffiti Alley, ON',
     43.64807490885711,
     -79.39846217344689,
@@ -100,7 +100,7 @@ CREATE TABLE IF NOT EXISTS reviews (
     REFERENCES activities (activity_id)
   );
 
-  INSERT INTO reviews(user_id, activity_id, review, rating) VALUES (3,1,'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 4),(2,1,'Fusce et nulla egestas, tempus felis in, tempus ipsum.',5),(1,1,'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer aliquam finibus dolor, vitae placerat tortor porta id. Fusce et nulla egestas, tempus felis in, tempus ipsum. Donec a urna maximus, maximus sapien id, elementum metus. Integer faucibus rutrum lacus, egestas posuere augue malesuada ut. Proin semper risus sit amet nulla tristique, ut lobortis tellus imperdiet. Cras egestas orci quis mauris tincidunt tempus. Nullam sodales nunc purus, ac varius ante gravida vel. Sed eu turpis dapibus, vestibulum erat quis, sagittis urna. In lobortis urna a tincidunt dictum. Etiam sit amet libero sed diam ultricies imperdiet. In hac habitasse platea dictumst.', 3);
+  INSERT INTO reviews(user_id, activity_id, review, rating) VALUES (3,1,'This is a beautiful park where dogs can be off-leash. There are enough benches to sit, rest and read a good book', 4),(3,1,'Amazing park with cute and energetic dogs. The place is beside a huge trail to make a hiking',5),(2,1,'I had a great time in Prince Edward Island and I really recommend taking a car and going along the scenic view', 3);
 
 CREATE TABLE IF NOT EXISTS tags (
   tags_id SERIAL,
